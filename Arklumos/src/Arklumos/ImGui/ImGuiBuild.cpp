7db578c2b6ae6514f42b2fd614b4e1f3015b5d5d
jmalensen <contact.wadj0s@gmail.com>
2023-03-02 11:15:22 +0100

    Small update ImGuiBuild

--- Arklumos/src/Arklumos/ImGui/ImGuiBuild.cpp
+++ Arklumos/src/Arklumos/ImGui/ImGuiBuild.cpp
@@ -1,6 +1,7 @@
 #include "akpch.h"
 
+#define _CRT_SECURE_NO_WARNINGS
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
 // #define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
-// #include "backends/imgui_impl_glfw.cpp"
-// #include "backends/imgui_impl_opengl3.cpp"
+#include "backends/imgui_impl_opengl3.cpp"
+#include "backends/imgui_impl_glfw.cpp"

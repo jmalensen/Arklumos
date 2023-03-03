ea6225fbff2ea6c53fd3904b773a1b95baf04334
jmalensen <contact.wadj0s@gmail.com>
2023-03-03 23:19:33 +0100

    One shader to rule them all

--- Arklumos/src/Arklumos/Renderer/Texture.cpp
+++ Arklumos/src/Arklumos/Renderer/Texture.cpp
@@ -6,6 +6,21 @@
 
 namespace Arklumos
 {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			AK_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		}
+
+		AK_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
 
 	Ref<Texture2D> Texture2D::Create(const std::string &path)
 	{
@@ -16,7 +31,7 @@ namespace Arklumos
 			return nullptr;
 
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		}
 
 		AK_CORE_ASSERT(false, "Unknown RendererAPI!");

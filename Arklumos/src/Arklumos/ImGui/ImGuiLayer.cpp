3fe4d0525b7a4c27a996c3e8d2a1d8a63ec717de
jmalensen <contact.wadj0s@gmail.com>
2023-03-05 19:28:31 +0100

    Fix docking and other small issues

--- Arklumos/src/Arklumos/ImGui/ImGuiLayer.cpp
+++ Arklumos/src/Arklumos/ImGui/ImGuiLayer.cpp
@@ -102,9 +102,12 @@ namespace Arklumos
 
 	void ImGuiLayer::OnEvent(Event &e)
 	{
-		ImGuiIO &io = ImGui::GetIO();
-		e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
-		e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
+		if (m_BlockEvents)
+		{
+			ImGuiIO &io = ImGui::GetIO();
+			e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
+			e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
+		}
 	}
 
 	/*
